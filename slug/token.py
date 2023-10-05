6747559adec829650a45da70e0df0085b808868e
Steve Marin <steve.marin@gmail.com>
2023-10-05 12:20:08 -0500

    init and token

---
+++ slug/token.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from enum import Enum
+from typing import Any
+
+class TokenType(Enum):
+    LEFT_PAREN = "("
+    RIGHT_PAREN = ")"
+    LEFT_BRACE = "{"
+    RIGHT_BRACE = "}"
+    COMMA = ","
+    DOT = "."
+    MINUS = "-"  
+    PLUS = "+"
+    SEMICOLON = ";"
+    SLASH = "/"
+    STAR = "*"
+    UNDERSCORE = "_"
+
+    BANG = "!"
+    BANG_EQUAL = "!="
+    EQUAL = "="
+    EQUAL_EQUAL = "=="
+    GREATER = ">"
+    GREATER_EQUAL = ">="
+    LESS = "<"
+    LESS_EQUAL = "<="
+
+    AND = "and"
+    CLASS = "class"
+    ELSE = "else"
+    FALSE = "false"
+    FUN = "fun"
+    FOR = "for"
+    IF = "if"
+    NIL = "nil"
+    OR = "or"
+    PRINT = "print"
+    RETURN = "return"
+    SUPER = "super"
+    THIS = "this"
+    TRUE = "true"
+    VAR = "var"
+    WHILE = "while"
+
+    IDENTIFIER = "identifier"
+    STRING = "str"
+    NUMBER = "number"
+    INTEGER = "int"
+    FLOAT = "float"
+
+    EOF = "eof"
+
+
+class Token:
+    __slots__ = ("type", "lexeme", "literal", "line")
+
+    def __init__(self, type: TokenType, lexeme: str, literal: Any, line: int):
+        self.type: TokenType = type
+        self.lexeme: str = lexeme
+        self.literal: Any = literal
+        self.line: int = line
+
+    def __repr__(self):
+        return f"{self.type} {self.lexeme} {self.literal}"
